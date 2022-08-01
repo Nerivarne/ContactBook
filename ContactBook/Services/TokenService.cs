8fe85af0a230849fa1f7f8c902ac4e87a81bbce3
Nerivarne <sandrakubenova@gmail.com>
2022-08-01 14:50:19 +0200

    add register and login user

--- ContactBook/Services/TokenService.cs
+++ ContactBook/Services/TokenService.cs
@@ -1,18 +1,55 @@
-﻿using ContactBook.Interfaces;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using ContactBook.Interfaces;
 using ContactBook.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ContactBook.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly IConfiguration configuration;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly IUserService userService;
+        public TokenService(IConfiguration configuration, IHttpContextAccessor httpContextAccessor, IUserService userService)
+        {
+            this.configuration = configuration;
+            this.httpContextAccessor = httpContextAccessor;
+            this.userService = userService;
+        }
         public string CreateLoginToken(User user)
         {
-            throw new NotImplementedException();
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
 
         public User GetLoggedInUser()
         {
-            throw new NotImplementedException();
+            var identity = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
+
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+
+                return userService.GetUserById(Guid.Parse(userClaims.FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier).Value));
+            }
+            return null;
         }
     }
 }
--- ContactBook/Services/UserService.cs
+++ ContactBook/Services/UserService.cs
@@ -1,18 +1,43 @@
-﻿using ContactBook.Interfaces;
+﻿using ContactBook.Database;
+using ContactBook.Interfaces;
 using ContactBook.Models;
 
 namespace ContactBook.Services
 {
     public class UserService : IUserService
     {
+        private AppDbContext database;
+        public UserService(AppDbContext database)
+        {
+            this.database = database;
+        }
+
+        public User GetUserByEmail(string userEmail)
+        {
+            var searchedUser = database.Users.SingleOrDefault(u => u.Email == userEmail);
+            return searchedUser;
+        }
+
+        public User GetUserById(Guid userId)
+        {
+            var searchedUser = database.Users.SingleOrDefault(u => u.Id == userId);
+            return searchedUser;
+        }
+
         public ResponseMessage CreateNewUser(UserRegisterDTO user)
         {
-            throw new NotImplementedException();
+            User newUser = new User(user.Email, user.Password);
+            database.Users.Add(newUser);
+            database.SaveChanges();
+            return new ResponseMessage()
+            {
+                Message = "User has been registered."
+            };
         }
 
         public bool DoesUserEmailExist(string userEmail)
         {
-            throw new NotImplementedException();
+            return database.Users.Where(u => u.Email == userEmail).Any();
         }
     }
 }
